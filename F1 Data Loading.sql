a24539ddf370add2b91b6431f6ad8a318e5f3192
Andrew Braun <57797832+BraunAndrewOMA@users.noreply.github.com>
2021-06-29 06:43:34 -0500

    Update F1 Data Loading.sql

--- F1 Data Loading.sql
+++ F1 Data Loading.sql
@@ -1,69 +1,69 @@
 COPY circuits
-FROM 'C:\Users\Braun Family\Documents\CSU Global\MIS581 Capstone\F1 Data\circuits.csv'
+FROM 'C:\Users\...\F1 Data\circuits.csv'
 DELIMITER ','
 CSV HEADER;
 
 COPY constructor_results
-FROM 'C:\Users\Braun Family\Documents\CSU Global\MIS581 Capstone\F1 Data\constructor_results.csv'
+FROM 'C:\Users\...\F1 Data\constructor_results.csv'
 DELIMITER ','
 CSV HEADER;
 
 COPY constructor_standings
-FROM 'C:\Users\Braun Family\Documents\CSU Global\MIS581 Capstone\F1 Data\constructor_standings.csv'
+FROM 'C:\Users\...\F1 Data\constructor_standings.csv'
 DELIMITER ','
 CSV HEADER;
 
 COPY constructors
-FROM 'C:\Users\Braun Family\Documents\CSU Global\MIS581 Capstone\F1 Data\constructors.csv'
+FROM 'C:\Users\...\F1 Data\constructors.csv'
 DELIMITER ','
 CSV HEADER;
 
 COPY driver_standings
-FROM 'C:\Users\Braun Family\Documents\CSU Global\MIS581 Capstone\F1 Data\driver_standings.csv'
+FROM 'C:\Users\...\F1 Data\driver_standings.csv'
 DELIMITER ','
 CSV HEADER;
 
 COPY drivers
-FROM 'C:\Users\Braun Family\Documents\CSU Global\MIS581 Capstone\F1 Data\drivers.csv'
+FROM 'C:\Users\...\F1 Data\drivers.csv'
 DELIMITER ','
 CSV HEADER;
 
 COPY lap_times
-FROM 'C:\Users\Braun Family\Documents\CSU Global\MIS581 Capstone\F1 Data\lap_times.csv'
+FROM 'C:\Users\...\F1 Data\lap_times.csv'
 DELIMITER ','
 CSV HEADER;
 
 COPY pit_stops
-FROM 'C:\Users\Braun Family\Documents\CSU Global\MIS581 Capstone\F1 Data\pit_stops.csv'
+FROM 'C:\Users\...\F1 Data\pit_stops.csv'
 DELIMITER ','
 CSV HEADER;
 
 COPY qualifying
-FROM 'C:\Users\Braun Family\Documents\CSU Global\MIS581 Capstone\F1 Data\qualifying.csv'
+FROM 'C:\Users\...\F1 Data\qualifying.csv'
 DELIMITER ','
 CSV HEADER;
 
 COPY races
-FROM 'C:\Users\Braun Family\Documents\CSU Global\MIS581 Capstone\F1 Data\races.csv'
+FROM 'C:\Users\...\F1 Data\races.csv'
 DELIMITER ','
 CSV HEADER;
 
 COPY results
-FROM 'C:\Users\Braun Family\Documents\CSU Global\MIS581 Capstone\F1 Data\results.csv'
+FROM 'C:\Users\...\F1 Data\results.csv'
 DELIMITER ','
 CSV HEADER;
 
 COPY seasons
-FROM 'C:\Users\Braun Family\Documents\CSU Global\MIS581 Capstone\F1 Data\seasons.csv'
+FROM 'C:\Users\...\F1 Data\seasons.csv'
 DELIMITER ','
 CSV HEADER;
 
 COPY status
-FROM 'C:\Users\Braun Family\Documents\CSU Global\MIS581 Capstone\F1 Data\status.csv'
+FROM 'C:\Users\...\F1 Data\status.csv'
 DELIMITER ','
 CSV HEADER;
 
 COPY engines
-FROM 'C:\Users\Braun Family\Documents\CSU Global\MIS581 Capstone\F1 Data\engines.csv'
+FROM 'C:\Users\...\F1 Data\engines.csv'
 DELIMITER ','
 CSV HEADER;
